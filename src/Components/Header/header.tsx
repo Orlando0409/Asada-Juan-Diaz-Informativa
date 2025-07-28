22ca1629735a6b0c81bed03e8fbb928ca097bade
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-07-28 17:46:00 -0600

    .

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import Navbar from './navbar'
+import Navbar from './Navbar'
+import Data from '../../data/Data.json'
+import { Link } from '@tanstack/react-router'
 
 const header = () => {
   return (
-    <header>
-      <img src="/Logo_ASADA_Juan_Díaz.png" alt="Logo ASADA Juan Díaz" />
+    <header className="flex justify-between items-center p-4 bg-blue-500 text-white z-50 md:hiden">
+      <Link to={Data.header.logo.ruta} className="flex items-center">
+        <img src="/Logo_ASADA_Juan_Díaz.png" alt="Logo ASADA Juan Díaz" className="h-12" />
+      </Link>
       <Navbar />
+      <Link to={Data.header.IncioSesion.Ruta} className='bg-white text-gray-800 px-4 py-2 rounded-md hover:bg-gray-600 hover:text-white'>{Data.header.IncioSesion.Texto}</Link>
     </header>
   )
 }
