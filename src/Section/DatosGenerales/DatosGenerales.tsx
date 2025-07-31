6fdc996f559e1970eca82ccc665de6be6fc6c935
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-07-31 10:07:02 -0600

    no se puede tener un p dentro de otro p jajjaj

--- src/Section/DatosGenerales/DatosGenerales.tsx
+++ src/Section/DatosGenerales/DatosGenerales.tsx
@@ -6,10 +6,10 @@ const DatosGenerales = () => {
       <div className='flex flex-col sm:gap-3 lg:gap-5'>
        <h1 className='font-bold mb-4 sm:text-sm md:text-md lg:text-2xl'>Datos Generales</h1>
         <blockquote className='border-l-4 border-[#6FCAF1] pl-4'>
-            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><p className='font-semibold'>Nombre:</p> {nombre}</p>
-            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><p className='font-semibold'>Servicios:</p> {servicios.descripcion}</p>
-            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><p className='font-semibold'>Ubicación:</p> {ubicacion}</p>
-            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><p className='font-semibold'>Año de Fundación:</p> {añoFundacion}</p>
+            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><span className='font-semibold'>Nombre:</span> {nombre}</p>
+            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><span className='font-semibold'>Servicios:</span> {servicios.descripcion}</p>
+            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><span className='font-semibold'>Ubicación:</span> {ubicacion}</p>
+            <p className='p-4 sm:text-md md:text-lg lg:text-xl flex gap-2'><span className='font-semibold'>Año de Fundación:</span> {añoFundacion}</p>
             <p className='p-4 sm:text-md md:text-lg lg:text-xl italic'>{descripcion}</p>
         </blockquote>
       </div>
