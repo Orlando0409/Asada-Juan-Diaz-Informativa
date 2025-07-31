6c5f5b313c95e1a673789a0de3931037419380d7
PaolaAlvarez28 <161152689+PaolaAlvarez28@users.noreply.github.com>
2025-07-31 09:12:28 -0600

    Hero, Resumen Mision Vision

--- src/Section/Hero.tsx
+++ src/Section/Hero.tsx
@@ -1,6 +1,23 @@
+import React from 'react'
+import Data from '../data/Data.json'
 const Hero = () => {
+const { imagen,titulo1, titulo2, subtitulo}= Data.header.hero;
   return (
-    <div>Hero</div>
+   <div className='w-full h-[100vh] overflow-hidden'>
+        <img alt='Historia de la ASADA de Juan Díaz'
+          src={imagen}
+          className='w-full h-full object-cover'
+        />
+
+<div className="absolute inset-0  flex  flex-col items-center justify-center text-center px-7">
+  <h1 className="text-white text-5xl font-extrabold">{titulo1}</h1>
+  <h1 className="text-white text-5xl font-extrabold">{titulo2}</h1>
+
+  <p className="text-white text-lg pt-8">{subtitulo} </p>
+</div>
+
+      </div>
+   
   )
 }
 
