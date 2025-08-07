77e291e61f44c088c7faec09a153d19b56105306
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-08-07 14:56:53 -0600

    ajustes del logo y footer

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -14,8 +14,8 @@ const Header = () => {
   return (
     <nav className='fixed top-0 w-screen z-50 bg-white border-b border-gray-200 px-4 md:px-8 py-3 flex items-center justify-between shadow-sm'>
       {/* Logo */}
-      <Link to={logo.ruta} className='flex items-center gap-2'>
-        <img src={logo.src} alt='logo' className='h-12 w-auto object-contain' />
+      <Link to={logo.ruta} className='flex items-center gap-2 h-13 w-15'>
+        <img src={logo.src} alt='logo' className='h-full w-full' />
       </Link>
       {/* Header Desktop */}
       <DesktopHeader 
