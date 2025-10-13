0b2bc368ab056a0f23c14cd168e0dd9e7a394ba3
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-10-13 17:12:24 -0600

    cositas

--- src/Section/Afiliacion/DesconexionMedidor.tsx
+++ src/Section/Afiliacion/DesconexionMedidor.tsx
@@ -107,7 +107,7 @@ const DesconexionMedidor = () => {
                          {/* Contenedor del formulario */}
                          <div className="rounded relative w-[95%] sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] max-w-5xl max-h-[80vh] sm:max-h-[85vh] md:max-h-[550px] overflow-y-scroll scrollbar-hide"
                               style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
-                              <DesconexionMedidorJuridica tipo="desconexion" onClose={() => setMostrarFormularioJuridico(false)} />
+                              <DesconexionMedidorJuridica onClose={() => setMostrarFormularioJuridico(false)} />
                          </div>
                     </div>
                )}
@@ -123,7 +123,7 @@ const DesconexionMedidor = () => {
                          {/* Contenedor del formulario */}
                          <div className="rounded relative w-[95%] sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] max-w-5xl max-h-[80vh] sm:max-h-[85vh] md:max-h-[550px] overflow-y-scroll scrollbar-hide"
                               style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
-                              <FormularioDesconexion tipo="desconexion" onClose={() => setMostrarFormularioFisico(false)} />
+                              <FormularioDesconexion onClose={() => setMostrarFormularioFisico(false)} />
                          </div>
                     </div>
                )}
