902f8fdb04efc2f67b1aa0d3fc9b855203c26e85
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-10-31 16:13:38 -0600

    listo

--- src/api/apiAuth.ts
+++ src/api/apiAuth.ts
@@ -5,7 +5,7 @@ const apiAuth = axios.create({
   //URL API PRODUCCION =https://saga-jd-back-end-1.onrender.com/api
   //URL API LOCAL = http://localhost:3000/api
 
-  baseURL: "http://localhost:3000/api", 
+  baseURL: "https://saga-jd-back-end-1.onrender.com/api", 
 
 });
 
